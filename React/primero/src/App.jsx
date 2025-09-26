c0c09d708f6eb51521ffb33a19974949b458bd55
José Ángel <jangelfa26@educastur.es>
2025-09-26 12:54:14 +0200

    prueba de react con bootstrap

--- React/primero/src/App.jsx
+++ React/primero/src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>primero segundo</h1>
+    <div className="container">
+      <h1>primero</h1>
+      <p>párrafo</p>
+      <p>Otro párrafo</p>
+      <button type="button" className="btn btn-danger">Primary</button>
+      
+      <form class="row g-3">
+        <div class="col-auto">
+          <label for="staticEmail2" class="visually-hidden">Email</label>
+          <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="email@example.com"/>
+        </div>
+        <div class="col-auto">
+          <label for="inputPassword2" class="visually-hidden">Password</label>
+          <input type="password" class="form-control" id="inputPassword2" placeholder="Password"/>
+        </div>
+        <div class="col-auto">
+          <button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
+        </div>
+      </form>
+      </div>
     </>
   )
 }
---
+++ React/primero/src/Dos.jsx
@@ -0,0 +1,19 @@
+//import './App.css'
+
+function Dos() {
+
+    return (
+        <>
+            <div className="container">
+                <h1>Segundo</h1>
+                <ul>
+                    <li>uno</li>
+                    <li>dos</li>
+                    <li>tres</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Dos
